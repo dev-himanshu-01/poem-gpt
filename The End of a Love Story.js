c9b9c42abe4e8234ae22b63c15640e2a1896965e
dev-himanshu-01 <himanshu01.dev@gmail.com>
2023-04-09 08:26:33 -0700

    'the end of a love story' :added

---
+++ The End of a Love Story.js
@@ -0,0 +1,28 @@
+/* 
+Heart shattered and bruised,
+All my feelings and trust abused,
+Once in love with a girl so kind,
+But she left me, love blind.
+
+My heart yearned for her every day,
+And my love for her wouldn't sway,
+I was devoted, I was true,
+But she betrayed me, what could I do?
+
+I gave her all my time and care,
+But in the end, she didn't seem to care,
+She chose another, leaving me behind,
+Leaving me with memories, so unkind.
+The love I had, now turned to dust,
+Betrayed by someone I thought I could trust.
+
+Now I am left with a broken heart,
+And every day is a brand new start,
+To try to mend and move ahead,
+And try to forget the love that's dead.
+
+But in my heart, I'll always keep,
+The memories of a love so deep,
+The love I had for a girl so true,
+But who left me alone, feeling blue.
+*/
